be568f13f633bbde2bbfec49332fa68568d2907f
Tomarr <rahul.0187@gmail.com>
2017-09-04 20:45:21 -0500

    First Commit

---
+++ FlightSearch/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.Web.Mvc;
+using CsvHelper;
+using FlightSearch.Models;
+
+namespace FlightSearch.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var csvAirports =System.IO.File.ReadAllText(Server.MapPath(@"~/CSVFiles/airports.csv"));
+          TextReader txtReader= new StringReader(csvAirports);
+            var csv = new CsvReader(txtReader); 
+            var records = csv.GetRecords<AirPort>().ToList();
+            ViewBag.AirportsFrom = records; 
+            ViewBag.AirportsTo = records;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SearchFlights(string from, string to,string sort="")
+        { 
+            
+            //loading the Flights csv file 
+            var csvFlights = System.IO.File.ReadAllText(Server.MapPath(@"~/CSVFiles/flights.csv"));
+            TextReader txtReader = new StringReader(csvFlights);
+            var csv = new CsvReader(txtReader);
+            // apply search parameter to the list 
+            var records = csv.GetRecords<Flight>().Where(m=> m.From== from && m.To==to);
+            
+            
+            if (sort == "departs")
+                records = records.OrderBy(m => m.Departs);
+            else if (sort == "arrives")
+                records = records.OrderBy(m => m.Arrives);
+            else if (sort == "FirstClassPrice")
+                records = records.OrderBy(m => m.FirstClassPrice);
+            else if (sort == "MainCabinPrice")
+                records = records.OrderBy(m => m.MainCabinPrice);
+ 
+            return Json(records);
+        }
+    }
+}
---
+++ FlightSearch/Models/Flight.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlightSearch.Models
+{
+    public class Flight
+    {
+        //From,To,FlightNumber,Departs,Arrives,MainCabinPrice,FirstClassPrice
+        public string From { get; set; }
+
+        public string To { get; set; }
+        public int FlightNumber { get; set; }
+        public DateTime Departs { get; set; }
+        public DateTime Arrives { get; set; }
+        public float MainCabinPrice { get; set; }
+        public float FirstClassPrice { get; set; }
+    }
+}
